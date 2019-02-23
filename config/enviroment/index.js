c2ebdd29be18125f55cea2b1ac16a49b073339d8
IlarionHalushka <hilarion.galushka@gmail.com>
2019-02-23 18:52:31 +0200

    add config env

---
+++ config/enviroment/index.js
@@ -0,0 +1,21 @@
+import development from './development';
+import test from './test';
+import production from './production';
+
+let environmentPath;
+
+switch (process.env.ENVIRONMENT) {
+  case 'development':
+    environmentPath = development;
+    break;
+  case 'test':
+    environmentPath = test;
+    break;
+  case 'production':
+    environmentPath = production;
+    break;
+  default:
+    environmentPath = development;
+}
+
+module.exports = environmentPath;
