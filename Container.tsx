865bc9216851790c0f3ca7219dce1f51c8a40a6d
IlarionHalushka <hilarion.galushka@gmail.com>
2019-05-01 23:30:50 +0300

    add menu icon to open drawer button

--- Container.tsx
+++ Container.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 import { Component } from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Button } from "native-base";
+
+import { Ionicons } from '@expo/vector-icons';
 
 export default class Container extends Component {
   render() {
     return (
       <View style={[styles.container, this.props.style]}>
-        <Button onPress={this.props.navigation.openDrawer}>
-          <Text>Open Drawer</Text>
+        <Button onPress={this.props.navigation.openDrawer} transparent>
+          <Ionicons style={{ paddingHorizontal: 5 }} name="ios-menu" size={32} color="blue" />
         </Button>
         {this.props.children}
       </View>
