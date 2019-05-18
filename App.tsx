04ee4613198a259cd8fef1f0c073960a8249a58e
IlarionHalushka <hilarion.galushka@gmail.com>
2019-05-18 23:29:43 +0300

    add bottom navigation tabs in Vacancies stack

--- App.tsx
+++ App.tsx
@@ -1,56 +1,80 @@
-import React from "react";
+import React from 'react';
 import {
-  createDrawerNavigator,
   createAppContainer,
-  createStackNavigator
-} from "react-navigation";
-import Qualifications from "./Qualifications";
-import Vacancies from "./Vacancies";
-import Settings from "./Settings";
+  createDrawerNavigator,
+  createMaterialTopTabNavigator,
+  createStackNavigator,
+} from 'react-navigation';
+
+import Qualifications from './Qualifications';
+import Settings from './Settings';
+import Vacancies from './Vacancies';
 
 const VacanciesStack = createStackNavigator(
   {
     Vacancies: {
-      screen: Vacancies
-    }
+      screen: Vacancies,
+    },
   },
   {
-    initialRouteName: "Vacancies"
+    initialRouteName: 'Vacancies',
   }
 );
 
 const QualificationsStack = createStackNavigator(
   {
     Qualifications: {
-      screen: Qualifications
-    }
+      screen: Qualifications,
+    },
   },
   {
-    initialRouteName: "Qualifications"
+    initialRouteName: 'Qualifications',
   }
 );
 
 const SettingsStack = createStackNavigator(
   {
     Settings: {
-      screen: Settings
-    }
+      screen: Settings,
+    },
   },
   {
-    initialRouteName: "Settings"
+    initialRouteName: 'Settings',
+  }
+);
+
+const VacanciesTabs = createMaterialTopTabNavigator(
+  {
+    Vacancies: { screen: VacanciesStack },
+    Settings: { screen: SettingsStack },
+  },
+  {
+    initialRouteName: 'Vacancies',
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+      labelStyle: {
+        fontSize: 12,
+      },
+      tabStyle: {
+        width: 100,
+      },
+      style: {
+        backgroundColor: 'blue',
+      },
+    },
   }
 );
 
 const MyDrawerNavigator = createDrawerNavigator({
   Qualifications: {
-    screen: QualificationsStack
+    screen: QualificationsStack,
   },
   Vacancies: {
-    screen: VacanciesStack
+    screen: VacanciesTabs,
   },
   Settings: {
-    screen: SettingsStack
-  }
+    screen: SettingsStack,
+  },
 });
 
 export default createAppContainer(MyDrawerNavigator);
