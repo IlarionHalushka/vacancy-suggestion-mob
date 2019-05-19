147958e59cdfbc2771a7807478ea9ca684df72ab
IlarionHalushka <hilarion.galushka@gmail.com>
2019-05-19 21:04:59 +0300

    fix tsling problems for TabContainer

--- TabContainer.tsx
+++ TabContainer.tsx
@@ -1,12 +1,6 @@
-import * as React from "react";
-import {
-  Text,
-  StyleSheet,
-  FlatList,
-  ScrollView,
-  RefreshControl
-} from "react-native";
-import { Card } from "native-base";
+import { Card } from 'native-base';
+import * as React from 'react';
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text } from 'react-native';
 
 interface IItem {
   vacancyId: string;
@@ -19,29 +13,19 @@ interface ITheme {
 
 interface IProps {
   isRefreshing: boolean;
-  onRefresh: Function;
-  data: Array<Object>;
-  renderRow: Function;
+  data: IItem[];
   theme: ITheme;
+  style: ITheme;
+  onRefresh(): void;
+  renderRow(c: any, t: any): any;
+  loadData(): void;
 }
 
-const TabContainer = ({
-  isRefreshing,
-  onRefresh,
-  data,
-  renderRow,
-  theme
-}: IProps) => {
+const TabContainer = ({ isRefreshing, onRefresh, data, renderRow, theme }: IProps) => {
   return (
-    <ScrollView
-      refreshControl={
-        <RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />
-      }
-    >
+    <ScrollView refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh} />}>
       {!data.length ? (
-        <Text style={[styles.noDataText, theme]}>
-          No data. Scroll down to refresh
-        </Text>
+        <Text style={[styles.noDataText, theme]}>No data. Scroll down to refresh</Text>
       ) : (
         <Card style={theme}>
           <FlatList
@@ -57,28 +41,28 @@ const TabContainer = ({
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   listItem: {
     margin: 2,
-    marginHorizontal: "5%",
+    marginHorizontal: '5%',
     borderTopWidth: 1,
     borderTopEndRadius: 10,
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   loaderText: {
-    color: "blue",
-    fontSize: 24
+    color: 'blue',
+    fontSize: 24,
   },
   actionButtonText: { fontSize: 20 },
   noDataText: {
     margin: 10,
-    alignSelf: "center"
+    alignSelf: 'center',
   },
-  tableText: { alignSelf: "center" },
-  tabs: { marginTop: 50 }
+  tableText: { alignSelf: 'center' },
+  tabs: { marginTop: 50 },
 });
 
 export default TabContainer;
